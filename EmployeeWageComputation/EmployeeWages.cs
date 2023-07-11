f03b092a55747724d2d1ccb18b6a40746b5619aa
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-11 10:48:23 +0530

    [Narenthra Kishore] Add: Employee Attendance Feature added

---
+++ EmployeeWageComputation/EmployeeWages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWages
+    {
+        public void EmployeeAttendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0 )
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is present");
+            
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWages employeeWages = new EmployeeWages();
+            employeeWages.EmployeeAttendance();
+        }
+    }
+}
