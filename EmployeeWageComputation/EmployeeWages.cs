dca7639775a0917ca38e321949ce1b4743273344
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-12 10:32:16 +0530

    [Narenthra Kishore] Add : New Feature total woking Hours

--- EmployeeWageComputation/EmployeeWages.cs
+++ EmployeeWageComputation/EmployeeWages.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageComputation
 {
     class EmployeeWages
     {
-        const int WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 4, IS_FULL_TIME = 1, IS_PART_TIME = 0 , TOTAL_WORKING_DAYS=20;
+        const int WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 4, IS_FULL_TIME = 1, IS_PART_TIME = 0 , TOTAL_WORKING_DAYS = 20, TOTAL_WORKING_HOURS = 100;
         Random random = new Random();
 
         public void EmployeeAttendance()
@@ -25,14 +25,14 @@ namespace EmployeeWageComputation
 
             int empCheck = random.Next(0,3);
             int empHrs = 0;
-            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
+            for (int i = 0; i < TOTAL_WORKING_DAYS && empHrs<TOTAL_WORKING_HOURS; i++)
             {
                 switch (empCheck)
                 {
-                    case 0:
+                    case IS_FULL_TIME:
                         empHrs += FULL_TIME_HR;
                         break;
-                    case 1:
+                    case IS_PART_TIME:
                         empHrs += PART_TIME_HR;
                         break;
                     default:
